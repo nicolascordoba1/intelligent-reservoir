3f8294f60e9c46cf45109100b44ecc9cbb8a61a2
Nicolas <nicolascordoba97@hotmail.com>
2021-10-23 22:26:53 -0500

    Se agregó modal para botón de geological data

--- main.js
+++ main.js
@@ -13,3 +13,31 @@ function show(){
 function close(){
     mainMenu.style.display = 'none';
 }
+
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.querySelector('.overlay')
+
+openModalButtons.forEach(button => {
+    button.addEventListener('click', () =>{
+        const modal = document.querySelector(button.dataset.modalTarget)
+        openModal(modal)
+    } )
+})
+closeModalButtons.forEach(button => {
+    button.addEventListener('click', () =>{
+        const modal = button.closest('.modal')
+        closeModal(modal)
+    } )
+})
+
+function openModal(modal){
+    if(modal==null) return
+    modal.classList.add('active')
+    overlay.classList.add('active')
+}
+function closeModal(modal){
+    if(modal==null) return
+    modal.classList.remove('active')
+    overlay.classList.remove('active')
+}
